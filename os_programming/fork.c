1073e5ecbc9388fb7a5b61c6fa583e5724e1cff2
varalu <varalupoola@gmail.com>
2024-03-05 12:17:28 +0000

    fork.c

---
+++ os_programming/fork.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include <unistd.h>
+
+int main ()
+{
+	int pid;
+
+	pid = fork();
+
+	if(pid == 0){
+		printf("child process retrun value:%d\n",pid);
+	}
+
+		else{
+			printf("parent process retrun value:%d\n",pid);
+
+		}
+
+		while (1);
+
+
+
+	return 0;
+}	
