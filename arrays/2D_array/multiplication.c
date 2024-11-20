381e0e3deee74c03615e248dac2539f36e3a5700
varalu <varalupoola@gmail.com>
2024-11-20 06:55:56 +0000

    arrays

---
+++ arrays/2D_array/multiplication.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+     int a[2][3] = {{1,2,3},{4,5,6}};
+int b[2][3]= {{2,4,6},{5,3,8}};
+
+ int c[2][3],i,j;
+	
+	
+	for(i=0;i<2;i++){
+	    
+	
+		for(j=0;j<3;j++){
+		    
+		
+			c[i][j]=a[i][j] * b[i][j];
+
+			printf("%d\t",c[i][j]);
+
+}
+printf("\n");
+}
+
+    return 0;
+}
+
